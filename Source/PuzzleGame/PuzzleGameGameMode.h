5dd040998d6fc112e4cd96018012fbac11dc1fe1
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2018-09-01 15:26:31 +0200

    Initial commit

---
+++ Source/PuzzleGame/PuzzleGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzleGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APuzzleGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APuzzleGameGameMode();
+};
+
+
+
